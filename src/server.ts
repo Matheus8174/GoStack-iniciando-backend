a1f7343994eabec04ba0e99f29e215c8adf12cf3
Matheus8174 <souzamatheus8174@gmail.com>
2021-07-07 20:12:54 -0300

    refactor: update the main file server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
-import express from 'express';
-import routes from './routes';
-
+import 'reflect-metadata';
 import './database/connection';
+import './config/env';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -10,6 +15,23 @@ const port = process.env.PORT || 3000;
 app.disable('x-powered-by');
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    });
+  }
+);
 
 app.listen(port);
