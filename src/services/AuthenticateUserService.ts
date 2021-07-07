8825e47a418a1b4bf0ee10b0d9d95ff034cf89cb
Matheus8174 <souzamatheus8174@gmail.com>
2021-07-07 19:50:23 -0300

    feat: create AuthenticateUserService Service

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+import AppError from '../errors/AppError';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const token = sign({}, process.env.SECRET, {
+      subject: user.id,
+      expiresIn: process.env.EXPIRESIN
+    });
+
+    return {
+      user,
+      token
+    };
+  }
+}
+
+export default AuthenticateUserService;
