acf0fa6787170b718e76b4595b81dbd64089e7b2
Matheus8174 <souzamatheus8174@gmail.com>
2021-07-07 20:12:23 -0300

    refactor: update the index router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+
 const route = Router();
 
-route.get('/', (request, response) => {
-  response.json({
-    hello: 'Olá'
-  });
-});
+route.use('/appointments', appointmentsRouter);
+route.use('/users', usersRouter);
+route.use('/sessions', sessionsRouter);
 
 export default route;
